31790617aebbd35e513cbf89d28d1aa58f0410cb
LAZ-R <crazydrawer62@gmail.com>
2024-10-06 02:23:28 +0200

    v 0.0.3 - change cachename

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'H3OPLUS2024';
+var cacheName = 'H3OPLUS2024it2';
 var filesToCache = [
   './',
   './index.html',
@@ -25,4 +25,4 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
-const version = '0.0.2';
+const version = '0.0.3';
